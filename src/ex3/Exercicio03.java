23154224560aa8ae2105f8df2a1080fadceb534f
Ricardoo <ricardo.tagami17@gmail.com>
2023-10-25 20:20:55 -0300

    Exercício 03

--- src/ex3/Exercicio03.java
+++ src/ex3/Exercicio03.java
@@ -1,8 +1,22 @@
 package ex3;
 
+import java.util.Scanner;
+
 public class Exercicio03 {
     public static void main(String[] args) {
 // Faça um programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra, escrever:
 // F - Feminino  M - Masculino  Sexo inválido
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite a letra F ou M: ");
+        String letter = scan.next();
+
+        if (!letter.equals("F") && !letter.equals("M")) {
+            System.out.println("Digite uma letra válida.");
+        } else if (letter.equals("F")) {
+            System.out.println("Feminino");
+        } else {
+            System.out.println("Masculino");
+        }
     }
 }
