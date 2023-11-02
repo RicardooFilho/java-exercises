bbb9d59ce1f738faf7bb119e3040ea95f84830d3
Ricardoo <ricardo.tagami17@gmail.com>
2023-11-02 19:27:38 -0300

    Exercicio 18

--- src/ex18/Exercicio18.java
+++ src/ex18/Exercicio18.java
@@ -1,5 +1,6 @@
 package ex18;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Exercicio18 {
@@ -13,15 +14,26 @@ entre outros. Exemplo:
 310,305, 301, 101, 311, 111, 25, 20, 10, 21, 11, 1, 7 e 16*/
         Scanner scan = new Scanner(System.in);
 
+        String[] numbers;
+
         Integer num = 0;
 
         do {
             System.out.println("Imprima um valor entre 1 e 1000");
             num = scan.nextInt();
-        } while (num >= 1 && num < 1000);
+        } while (num < 1 || num >= 1000);
 
-        String numStr = Integer.toString(num);
+        String numStr = num.toString();
 
-        numStr = numStr.split(num);
+        numbers = numStr.split("");
+
+        if (numbers.length == 3) {
+            if ()
+        } else if (numbers.length == 2) {
+        } else {
+        }
+//        System.out.println(numbers[0] + " centenas, " + numbers[1] + " dezenas e " + numbers[2] + " unidades");
+//        System.out.println(numbers[0] + " dezenas e " + numbers[1] + " unidades");
+//        System.out.println(numbers[0] + " unidades");
     }
 }
