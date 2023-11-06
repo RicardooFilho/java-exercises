c1cb5da5b11abdf15b49ac803401495b5623a56b
Ricardoo <ricardo.tagami17@gmail.com>
2023-11-06 19:40:34 -0300

    Exercicio 20

--- src/ex20/Exercicio20.java
+++ src/ex20/Exercicio20.java
@@ -1,5 +1,7 @@
 package ex20;
 
+import java.util.Scanner;
+
 public class Exercicio20 {
     public static void main(String[] args) {
 /*21. Faça um Programa para um caixa eletrônico. O programa deverá
@@ -11,5 +13,53 @@ Exemplo 1: Para sacar a quantia de 256 reais, o programa
 fornece duas notas de 100, uma nota de 50, uma nota de 5 e uma nota de 1;
 Exemplo 2: Para sacar a quantia de 399 reais, o programa fornece três notas de 100, uma nota de 50,
 quatro notas de 10, uma nota de 5 e quatro notas de 1.*/
+        Scanner scan = new Scanner(System.in);
+
+        Integer value = 0;
+
+        Integer hundred = 0;
+
+        Integer fifty = 0;
+
+        Integer ten = 0;
+
+        Integer five = 0;
+
+        Integer one = 0;
+
+        do {
+            System.out.println("Informe o valor que irá sacar: ");
+            value = scan.nextInt();
+        } while (value < 10 || value > 600);
+
+        if (value >= 100) {
+            hundred = value / 100;
+            value -= hundred * 100;
+            System.out.println("Notas de 100: " + hundred);
+        }
+
+        if (value >= 50) {
+            fifty = value / 50;
+            value -= fifty * 50;
+            System.out.println("Notas de 50: " + fifty);
+        }
+
+        if (value >= 10) {
+            ten = value / 10;
+            value -= ten * 10;
+            System.out.println("Notas de 10: " + ten);
+        }
+
+        if (value >= 5) {
+            five = value / 5;
+            value -= five * 5;
+            System.out.println("Notas de 5: " + five);
+        }
+
+        if (value >= 1) {
+            one = value / 1;
+            value -= one * 5;
+            System.out.println("Notas de 1: " + one);
+        }
     }
 }
